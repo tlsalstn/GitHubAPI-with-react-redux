97b6b03480b9ad3b0b2639a233161a2e93c044a5
QMCHE <39144276+QMCHE@users.noreply.github.com>
2020-07-17 17:12:29 +0900

    [ADD] Global Style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
 import React from 'react';
+import { createGlobalStyle } from "styled-components";
 import MainContainer from './containers/MainContainer/MainContainer';
+
+const GlobalStyle = createGlobalStyle`
+* {
+  box-sizing: border-box;
+  font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+  outline: none;
+}
+
+html,
+body,
+img {
+  margin: 0;
+  width: 100%;
+}
+
+body::-webkit-scrollbar {
+  width: 6px;
+}
+
+body::-webkit-scrollbar-track {
+  background: #383e49;
+}
+
+body::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+  background: #fff;
+}
+`;
 
 function App() {
   return (
     <div className="App">
-      <MainContainer />
+        <GlobalStyle />
+        <MainContainer />
     </div>
   );
 }
