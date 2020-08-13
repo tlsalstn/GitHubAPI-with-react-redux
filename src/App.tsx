39acd7451aef74e029b0f137870cba295911f377
QMCHE <39144276+QMCHE@users.noreply.github.com>
2020-08-13 20:51:19 +0900

    Revert "[ADD] Global Styles"
    This reverts commit 71aa28e249408b51ad634213ab6bf187f8d28079.

--- src/App.tsx
+++ src/App.tsx
@@ -2,41 +2,33 @@ import React from 'react';
 import { createGlobalStyle } from "styled-components";
 import MainContainer from './containers/MainContainer/MainContainer';
 
-const GlobalStyle = createGlobalStyle({
-  "*": {
-    margin: 0,
-    width: "100%",
-    boxSizing: "border-box",
-    fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif",
-    outline: "none"
-  },
-  "body::-webkit-scrollbar": {
-    width: "6px"
-  },
-  "body::-webkit-scrollbar-track": {
-    background: "#383e49"
-  },
-  "body::-webkit-scrollbar-thumb": {
-    borderRadius: "10px",
-    background: "#fff"
-  },
-  "p": {
-    width: "auto",
-    fontSize: "18px",
-    overflow: "hidden",
-    textOverflow: "ellipsis",
-    whiteSpace: "nowrap"
-  },
-  ".Modal": {
-    top: 0,
-    left: 0,
-    width: "100%",
-    height: "100%",
-    background: "rgba(0, 0, 0, 0.5)",
-    position: "fixed",
-    zIndex: 1000
-  }
-});
+const GlobalStyle = createGlobalStyle`
+* {
+  box-sizing: border-box;
+  font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+  outline: none;
+}
+
+html,
+body,
+img {
+  margin: 0;
+  width: 100%;
+}
+
+body::-webkit-scrollbar {
+  width: 6px;
+}
+
+body::-webkit-scrollbar-track {
+  background: #383e49;
+}
+
+body::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+  background: #fff;
+}
+`;
 
 function App() {
   return (
