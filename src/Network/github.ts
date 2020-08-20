fc9cfb1e4d0f33ab302fe4535c43dab7c1166d39
QMCHE <39144276+QMCHE@users.noreply.github.com>
2020-08-20 09:15:49 +0900

    [EDIT] Hide Token

--- src/Network/github.ts
+++ src/Network/github.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
+import { githubToken } from "../config/github"
 
 const instance = axios.create({
     baseURL: "https://api.github.com",
     headers: {
-        "Authorization": "token aa5201ea28c25e3b11a272f7fd1cd74b5e07c6e1",
+        "Authorization": "token " + githubToken,
         "Accept": "application/vnd.github.inertia-preview+json"
     }
 });
