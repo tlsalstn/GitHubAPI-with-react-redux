cbc5333bf907180706976e3fc77d9b03767ba025
QMCHE <39144276+QMCHE@users.noreply.github.com>
2020-08-21 17:13:58 +0900

    [UPDATE] Check login
    If logined, redirect user to home page
    else redirect login page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom"
+import LoginContainer from './containers/LoginContainer/LoginContainer'
 import MainContainer from './containers/MainContainer/MainContainer';
+import RedirectContainer from './containers/RedirectContainer/RedirectContainer';
 
 function App() {
   return (
     <Router>
-      <Route path="/" component={MainContainer} />
+      {window.localStorage.getItem('github_token') ?
+        <Switch>
+          <Route path="/home" component={MainContainer} />
+          <Redirect from="*" to="/home" />
+        </Switch>
+        :
+        <Switch>
+          <Route path="/login" component={LoginContainer} />
+          <Route path="/redirect" component={RedirectContainer} />
+          <Redirect to="/login" />
+        </Switch>
+      }
     </Router>
   );
 }
