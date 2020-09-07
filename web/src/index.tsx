cecb90ad3685fde313f48296824f02b4d5fe3f76
QMCHE <39144276+QMCHE@users.noreply.github.com>
2020-09-07 14:43:03 +0900

    [EDIT] Redux Devtools Option

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./modules";
 import GlobalStyle from "./styles/GlobalStyle"
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <React.StrictMode>
